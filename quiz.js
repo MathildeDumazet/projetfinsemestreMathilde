138d05c028b0ef3a7b04066efed8e0a6a1c503e4
MathildeDumazet <31658369+MathildeDumazet@users.noreply.github.com>
2017-11-28 16:02:29 +0100

    initalpush

---
+++ quiz.js
@@ -0,0 +1,44 @@
+var quiz = [
+  {
+    image: "./img/révélations.jpg",
+    question: "En quelle année la préselection des révélations a-t-elle été mise en place?",
+    choices: ["1997", "2007", "2017"],
+    answer: 1,
+    explainer: "La préselection des Révélations a été créé pour faciliter le choix des votants et pour mettre en valeur un plus grand nombre de jeunes acteurs et de jeunes actrices.",
+  },
+  {
+    image: "./img/camillecottin.jpg",
+    question: "Camille Cottin détient le record de la plus vieille actrice à avoir été nommée pour le César du meilleur espoir féminin. A quel âge?",
+    choices: ["32", "37", "39"],
+    answer: 1,
+    explainer : "Il n’y a pas de limite d’âge pour la catégorie du meilleur espoir. Pas de limite non plus quant au nombre de film tourné avant la nomination. Nommée à 35 ans Florence Loiret-Caille avait par exemple tourné plus de 20 long-métrages avant d’être sélectionnée.",
+  },
+  {
+    image: "./img/camera.jpg",
+    question: "Qui est le réalisateur ou la réalisatrice qui a indirectement obtenu le plus de prix dans la catégorie du meilleur espoir féminin ?",
+    choices: ["Abdellatif Kéchiche", "Céline Sciamma", "Bertrand Bonnello"],
+    answer: 0,
+    explainer : "Céline Sciamma et Bertrand Bonnello ont permis à quatre actrices d’être nommées parmi les révélations pour le césar du meilleur espoir féminin. Mais ce sont les films d’Abdellatif Kéchiche qui ont obtenu le plus de récompenses dans la catégorie : Hasia Herzi et Adèle Exarchopoulos ont toutes les deux obtenu leur statuette pour un film du réalisateur.",
+  },
+  {
+    image: "./img/charlotte gainsbourg.jpg",
+    question: "Quel est l’âge moyen des actrices récompensées dans la catégorie du meilleur espoir féminin? ",
+    choices: ["19", "23", "25"],
+    answer: 2,
+    explainer : "L’âge des révélations va de 16 à 37 ans mais la plupart d’entre elles ont autour de 25 ans. C’est aussi l’âge moyen des actrices récompensées.",    
+  },
+  {
+    image: "./img/filmographie.jpg",
+    question: "Dans combien de films (en moyenne) les actrices récompensées ont-elles joué avant d'obtenir leur prix?",
+    choices: ["2", "5", "8"],
+    answer: 1,
+    explainer : "Les parcours des révélations diffèrent : les jeunes actrices peuvent être débutantes ou confirmées. En moyenne, les jeunes actrice ont joué dans quatre films avant d’être nommées. Le chiffre monte à 5 pour les gagnantes.",
+  },
+  {
+    image: "./img/césar.jpg",
+    question: "Qui est l’actrice qui a le plus de chance de gagner cette année selon les statistiques?",
+    choices: ["Camélia Jordana", "Esther Garrel", "Ana Girardot"],
+    answer: 0,
+    explainer : "Camélia Jordana a 25 ans et six films à son actif. Elle tient le premier rôle dans Le Brio, un film réalisé par Yvan Attal. Si l’on ajoute que deux des gagnantes depuis cinq ans étaient également chanteuses (Louane Emera et Izia Higelin), Camélia Jordana a toutes les chances de gagner… du point de vue des statistiques.",
+  },
+];
